f03957185a477e951cc4b333c2bc73238f31cdf2
Matthew Zatzman <matthew.zatzman@gmail.com>
2022-12-29 12:43:02 -0600

    cleanup and update local build script

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # build
-docker build --platform linux/amd64 -t rstudio:4.2.2 .
+docker build --platform linux/amd64 -t rstudio:v0.3 .
 
-# tag
-docker image tag rstudio:4.2.2 zatzmanm/rstudio:4.2.2
+# # tag
+# docker image tag rstudio:4.2.2 zatzmanm/rstudio:4.2.2
 
-# push
-docker image push zatzmanm/rstudio:4.2.2
+# # push
+# docker image push zatzmanm/rstudio:4.2.2
 
