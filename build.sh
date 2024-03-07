587acb917172d1933cdc85582a958116e52ee022
Matthew Zatzman <matthew.zatzman@gmail.com>
2024-03-06 21:22:08 -0500

    gpu attempt

--- build.sh
+++
@@ -1,9 +0,0 @@
-# build
-docker build --platform linux/amd64 -t rstudio:v0.3 .
-
-# # tag
-# docker image tag rstudio:4.2.2 zatzmanm/rstudio:4.2.2
-
-# # push
-# docker image push zatzmanm/rstudio:4.2.2
-
